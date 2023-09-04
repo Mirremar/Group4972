20d78c0b05adce0ccea243961ade5ee807558344
Main Villain <deserteagle081@gmail.com>
2023-09-04 21:46:47 +0300

    created task 1,3

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Enter day of week");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine((week)a);
+enum week
+{
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+}
