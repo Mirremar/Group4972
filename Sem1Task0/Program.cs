97eeb3aca807c4686f2e389bbefdb43121fbbe60
Main Villain <deserteagle081@gmail.com>
2023-09-04 20:54:28 +0300

    created 1 task

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача:написать программу,принимающую на вход число и выдающую его квадрат
+Console.WriteLine("Введите число");
+//Считываем данные с консоли
+string? inputNum = Console.ReadLine();//??"0";
+//Проверяем,чтоб данные не были пустыми
+if (inputNum != null)
+{
+    //парсим введённое число
+    int num = int.Parse(inputNum);
+    //находим квадрат числа
+    int res = num * num;
+    //Выводим данные в консоль
+    Console.WriteLine(res);
+}
+
+Console.WriteLine("Квадрат числа: " + Math.Pow(int.Parse(inputNum),2));
