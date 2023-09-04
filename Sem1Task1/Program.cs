e3f6916cd5f487000f9948ad88e4ef9e1a544956
Main Villain <deserteagle081@gmail.com>
2023-09-04 21:08:35 +0300

    created 2 task

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numA = int.Parse(Console.ReadLine()??"0");
+int numB = int.Parse(Console.ReadLine()??"0");
+if (numA*numA == numB)
+{
+    Console.WriteLine("1 число — квадрат второго");
+
+}
+else
+{
+    Console.WriteLine("1 число — не квадрат второго");
+}
