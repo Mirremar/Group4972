cf50ec1b11212ae88554ab3f8504a2b9283f1c08
Main Villain <deserteagle081@gmail.com>
2023-09-06 20:58:11 +0300

    added Task 5

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number");
+int N = int.Parse(Console.ReadLine()??"0");
+int AntiN = -N;
+for (int i = 0; i <= 2*N; i++)
+{
+    if  (AntiN == N)
+    {
+        Console.Write(AntiN);
+        break;
+    }
+    Console.Write(AntiN + ", ");
+    AntiN += 1;
+    
+}
+
+//Можно сделать через цикл while,так проще и рациональнее.
