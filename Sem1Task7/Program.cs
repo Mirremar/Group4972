215b6fc204e7957cdca126eff4ae5b53e56b5528
Main Villain <deserteagle081@gmail.com>
2023-09-06 21:02:24 +0300

    added Task 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter 3digit number");
+int Number = int.Parse(Console.ReadLine()??"0");
+if (Number >= 100 && Number <= 1000)
+{
+    int Result = Number%10;
+    Console.WriteLine("Result is " + Result);
+}
+else
+{
+    Console.WriteLine("number is not a 3digit or it is negative");
+}
